cfd41851f47087ceecfec7cee390fb2b1c4ce167
Harrsh Patel <me.harrsh.patel@gmail.com>
2022-08-13 16:08:50 +0530

    Added Navbar

---
+++ components/ThemeToggler.js
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react';
+import { BsFillSunFill, BsFillMoonFill } from 'react-icons/bs';
+import { ThemeContext } from '../context/ThemeContext';
+import style from '../styles/Home.module.css';
+
+const ThemeToggler = () => {
+    const { theme, toggleTheme } = useContext(ThemeContext);
+
+    return (
+        <div onClick={toggleTheme} className={style.themeIconContainer}>
+            <BsFillSunFill
+                className={`${style.themeIcon} ${
+                    theme === 'light' ? style.hideIcon : style.showIcon
+                }`}
+            />
+            <BsFillMoonFill
+                className={`${style.themeIcon} ${
+                    theme === 'dark' ? style.hideIcon : style.showIcon
+                }`}
+            />
+        </div>
+    );
+};
+
+export default ThemeToggler;
--- pages/Home.js
+++ pages/Home.js
@@ -1,26 +1,12 @@
+import React from 'react';
+import ThemeToggler from '../components/ThemeToggler';
 import style from '../styles/Home.module.css';
-import React, { useContext } from 'react';
-import { FaMoon, FaSun } from 'react-icons/fa';
-import { ThemeContext } from '../context/ThemeContext';
 
 const Home = () => {
-    const { theme, toggleTheme } = useContext(ThemeContext);
-
     return (
         <div className={`${style.main}`}>
-            <div onClick={toggleTheme}>
-                <div className={style.themeIconContainer}>
-                    <FaSun
-                        className={`${style.themeIcon} ${
-                            theme === 'light' ? style.hideIcon : style.showIcon
-                        }`}
-                    />
-                    <FaMoon
-                        className={`${style.themeIcon} ${
-                            theme === 'dark' ? style.hideIcon : style.showIcon
-                        }`}
-                    />
-                </div>
+            <div className={style.navbar}>
+                <ThemeToggler />
             </div>
         </div>
     );
